0ed98751c1be4589f51a71463b5891f9b099680f
Skand Kulshrestha <skandkulshrestha@gmail.com>
2024-08-28 23:14:59 +0530

    Added fir1 filter example (currently not working)

--- SignalProcessing/filtering_examples.py
+++ SignalProcessing/filtering_examples.py
@@ -1,5 +1,4 @@
 import math
-from cProfile import label
 
 import numpy as np
 
@@ -32,13 +31,12 @@ class FIR:
 class FIRLS(FIR):
     SHAPE: Tuple = (0, 0, 1, 1, 0, 0)
 
-    def __init__(self, sampling_rate: int, frequency_range: Tuple[int, int], transition_width: float,
-                 order: int | None = None):
+    def __init__(self, sampling_rate: int, frequency_range: Tuple[int, int], transition_width: float, order: int):
         super().__init__(
             sampling_rate=sampling_rate,
             frequency_range=frequency_range,
             transition_width=transition_width,
-            order=round(5 * sampling_rate / frequency_range[0]) if order is None else order
+            order=order
         )
 
         # modify order to make odd since "signal.firls" take odd order value
@@ -70,16 +68,52 @@ class FIRLS(FIR):
         )
 
 
-class Evaluation:
-    def __init__(self, filter_kernel, nyquist_frequency):
-        self.filter_kernel = filter_kernel
-        self.nyquist_frequency = nyquist_frequency
+class FIR1(FIR):
+    SHAPE: Tuple = (0, 0, 1, 1, 0, 0)
 
+    def __init__(self, sampling_rate: int, frequency_range: Tuple[int, int], transition_width: float, order: int):
+        super().__init__(
+            sampling_rate=sampling_rate,
+            frequency_range=frequency_range,
+            transition_width=transition_width,
+            order=order
+        )
+
+        # modify order to make odd since "signal.firls" take odd order value
+        self.order = Utility.make_integer_odd_by_incrementing_by_one(self.order)
+
+        # calculate frequency vector using transition width
+        self.frequency_vector: np.array = np.array([
+            0,
+            self.frequency_range[0],
+            self.frequency_range[0],
+            self.frequency_range[1],
+            self.frequency_range[1],
+            self.nyquist_frequency
+        ])
+
+        # create filer kernel using "signal.firls"
+        self.filter_kernel: np.ndarray = signal.firwin(
+            numtaps=self.order,
+            cutoff=self.frequency_range,
+            fs=self.sampling_rate
+        )
+
+
+class Evaluation:
+    def __init__(self, filter_instance):
+        self.sampling_rate = filter_instance.sampling_rate
+        self.nyquist_frequency = filter_instance.nyquist_frequency
+        self.frequency_vector = filter_instance.frequency_vector
+        self.filter_kernel = filter_instance.filter_kernel
+
+        # output
         self.hz = None
         self.filter_power = None
         self.interpolated_hz = None
         self.interpolated_filter_power = None
         self.interpolated_ideal_power = None
+        self.pearson_correlation = 0
 
     def evaluate_kernel(self):
         # compute the power spectrum of the filter kernel
@@ -89,22 +123,29 @@ class Evaluation:
         # @WATCH: "Understanding the Discrete Fourier Transform and the FFT" @09:26
         #           https://www.youtube.com/watch?v=QmgJmh2I3Fw&t=145s
         self.hz = np.linspace(0, self.nyquist_frequency, math.floor(len(self.filter_kernel) / 2) + 1)
-        self.filter_power = filter_power[:len(self.hz)]
+        self.filter_power: np.ndarray = filter_power[:len(self.hz)]
+
+    def correlate(self, normalize):
+        interpolated_length = int(self.hz[-1] // normalize)
+        filter_power = np.zeros((interpolated_length,), dtype=self.filter_power.dtype)
+        ideal_power = np.zeros(filter_power.shape, dtype=filter_power.dtype)
+
+        for hz in range(interpolated_length):
+            original_hz = hz * normalize
+
+            # interpolate ideal power
+            pass
 
 
 def firls_ex1():
-    order = round(5 * 1024 / 20)
     firls = FIRLS(
         sampling_rate=1024,  # Hz
         frequency_range=(20, 45),
         transition_width=0.1,
-        order=order
+        order=round(5 * 1024 / 20)
     )
 
-    evaluation = Evaluation(
-        filter_kernel=firls.filter_kernel,
-        nyquist_frequency=firls.nyquist_frequency
-    )
+    evaluation = Evaluation(firls)
     evaluation.evaluate_kernel()
 
     plt.subplot(221)
@@ -133,5 +174,135 @@ def firls_ex1():
     plt.show()
 
 
+def firls_ex2():
+    print('Varying Order')
+    sampling_rate = 1024
+    frequency_range = (20, 45)
+    for i in range(1, 10):
+        firls = FIRLS(
+            sampling_rate=sampling_rate,  # Hz
+            frequency_range=frequency_range,
+            transition_width=0.1,
+            order=round(i * sampling_rate / frequency_range[0])
+        )
+
+        evaluation = Evaluation(firls)
+        evaluation.evaluate_kernel()
+        evaluation.correlate(1)
+        print(f'Order ({firls.order:3}) = {evaluation.pearson_correlation:.4f}')
+
+        plt.subplot(221)
+        plt.plot(np.arange(firls.order) - firls.order / 2, firls.filter_kernel + 0.02 * i, linestyle='-')
+
+        plt.subplot(222)
+        plt.plot(evaluation.hz, evaluation.filter_power, linestyle='-', label=f'Order = {firls.order}')
+
+        plt.subplot(224)
+        plt.plot(evaluation.hz, 10 * np.log10(evaluation.filter_power), linestyle='-')
+
+    plt.subplot(221)
+    plt.xlabel('Time points')
+    plt.title('Filter kernel (firls)')
+
+    plt.subplot(222)
+    plt.plot(firls.frequency_vector, firls.SHAPE, linestyle='--', label='Ideal')
+    plt.xlim(0, firls.frequency_range[0] * 5)
+    plt.legend()
+    plt.title('Frequency response of filter (firls)')
+    plt.ylabel('Filter gain')
+
+    plt.subplot(224)
+    plt.xlim(0, firls.frequency_range[0] * 5)
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Filter gain (dB)')
+
+    plt.show()
+
+
+def firls_ex3():
+    print('Varying Transition Width')
+    sampling_rate = 1024
+    frequency_range = (20, 45)
+    transition_widths = np.linspace(0.01, 0.4, 10)
+    for i, transition_width in enumerate(transition_widths):
+        firls = FIRLS(
+            sampling_rate=sampling_rate,  # Hz
+            frequency_range=frequency_range,
+            transition_width=transition_width,
+            order=round(5 * sampling_rate / frequency_range[0])
+        )
+
+        evaluation = Evaluation(firls)
+        evaluation.evaluate_kernel()
+
+        plt.subplot(221)
+        plt.plot(np.arange(firls.order) - firls.order / 2, firls.filter_kernel + 0.02 * i, linestyle='-')
+
+        plt.subplot(222)
+        plt.plot(evaluation.hz, evaluation.filter_power, linestyle='-', label=f'T.W. = {firls.transition_width:.2f}')
+
+        plt.subplot(224)
+        plt.plot(evaluation.hz, 10 * np.log10(evaluation.filter_power), linestyle='-')
+
+    plt.subplot(221)
+    plt.xlabel('Time points')
+    plt.title('Filter kernel (firls)')
+
+    plt.subplot(222)
+    plt.plot(firls.frequency_vector, firls.SHAPE, linestyle='--', label='Ideal')
+    plt.xlim(0, firls.frequency_range[0] * 5)
+    plt.legend()
+    plt.title('Frequency response of filter (firls)')
+    plt.ylabel('Filter gain')
+
+    plt.subplot(224)
+    plt.xlim(0, firls.frequency_range[0] * 5)
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Filter gain (dB)')
+
+    plt.show()
+
+
+def fir1_ex1():
+    firls = FIR1(
+        sampling_rate=1024,  # Hz
+        frequency_range=(20, 45),
+        transition_width=0.1,
+        order=round(5 * 1024 / 20)
+    )
+
+    evaluation = Evaluation(firls)
+    evaluation.evaluate_kernel()
+
+    plt.subplot(221)
+    plt.plot(firls.filter_kernel, color='blue', linestyle='-', linewidth=2)
+    plt.xlabel('Time points')
+    plt.title('Filter kernel (fir1)')
+
+    plt.subplot(222)
+    plt.plot(evaluation.hz, evaluation.filter_power, color='black', linestyle='-', marker='s', linewidth=2,
+             label='Actual')
+    plt.plot(firls.frequency_vector, firls.SHAPE, color='red', linestyle='-', marker='o',
+             linewidth=2, label='Ideal')
+    plt.xlim(0, firls.frequency_range[0] * 4)
+    plt.legend()
+    plt.title('Frequency response of filter (fir1)')
+    plt.ylabel('Filter gain')
+
+    plt.subplot(224)
+    plt.plot(evaluation.hz, 10 * np.log10(evaluation.filter_power), color='black', linestyle='-', marker='s',
+             linewidth=2)
+    plt.xlim(0, firls.frequency_range[0] * 4)
+    plt.ylim(-50, 2)
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Filter gain (dB)')
+
+    plt.show()
+
+
 if __name__ == '__main__':
-    firls_ex1()
+    # firls_ex1()
+    # firls_ex2()
+    # firls_ex3()
+
+    fir1_ex1()
